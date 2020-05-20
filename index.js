8dfcb125b22994656ab001ae043903aa2f2cc0f2
AldoEstebanPaz <estebanpazutn@gmail.com>
2020-05-20 18:51:18 -0300

    remove file listing

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ require('./config');
 require('./config-database');
 
 const express = require('express');
-const serveStatic = require('serve-static');
-const serveIndex = require('serve-index');
 const morgan = require('morgan');
 const apiV1Router = require('./src/api/v1');
 
@@ -12,8 +10,6 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 app.use('/api/v1', apiV1Router);
-app.use('/explorer', serveIndex(__dirname, { hidden: true, view: 'details', icons: true }));
-app.use('/explorer', serveStatic(__dirname, { index: false, dotfiles: 'allow' }));
 
 app.listen(process.env.PORT, () =>
   console.log(`The application is running on port ${process.env.PORT}`));
